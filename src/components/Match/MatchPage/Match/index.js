a4cadfbbb6270eca0be659a647463ba13521314a
Alejandro U. Alvarez <aluralma@gmail.com>
2019-03-06 13:54:09 +0000

    initial work for a centered progress bar that leans towards the winner

--- src/components/Match/MatchPage/Match/index.js
+++ src/components/Match/MatchPage/Match/index.js
@@ -11,6 +11,7 @@ export default (props) => {
     const colorB = stats.wins[1] > stats.wins[0] ? 'green' : null;
 
     let progress = null;
+    let winnerProgress = null;
     let gridStyle = null;
 
     if (props.displayProgress) {
@@ -18,17 +19,33 @@ export default (props) => {
             borderBottom: '1px solid #efefef',
         };
         progress = (
-            <Progress
-                value={ match.stats.gamesCompleted }
-                total={ totalGames }
-                progress='ratio'
-                indicating
-            />
+            <div className='progress-wrapper'>
+                <Progress
+                    value={ match.stats.gamesCompleted }
+                    total={ totalGames }
+                    attached='top'
+                />
+            </div>
+        );
+        // Display the progress towards winning by any of the two players
+        // this is a bar that begins in the center (tie), and moves towards
+        // the winner's side. If it hits the end, that player wins
+        // Winning just implies winning more games than the other player, so
+        // initially that's winning 50 games
+        const winMax = Math.max(stats.wins[0], stats.wins[1]);
+        const winnerBarMax = (totalGames - match.stats.gamesCompleted) / 2;
+        const winBarLength = winMax * 100 / winnerBarMax; // %
+
+        winnerProgress = (
+            <div className='progress-winner'>
+                <div></div>
+            </div>
         );
     }
     
     return (
         <div className={ classNames('match', { 'small': props.small }) }>
+            { progress }
             <Grid columns={ 2 } textAlign='center' verticalAlign='middle' style={ gridStyle }>
                 <Grid.Column color={ colorA }>
                     <h2>{ match.players[0].token }</h2>
@@ -39,7 +56,6 @@ export default (props) => {
                     <h3>{ stats.wins[1] }</h3>
                 </Grid.Column>
             </Grid>
-            { progress }
         </div>
     );
 };
